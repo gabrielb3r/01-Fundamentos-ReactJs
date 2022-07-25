3c9e97ccc39b045fdb60ca5d9b70e46b64825897
GABRIEL BERNARDO MARTINS - SEMMA <T000007@pmc.local>
2022-07-25 13:59:47 -0300

    Função de likes implementada

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,15 @@
 import styles from './Comment.module.css'
 import {Trash, ThumbsUp} from 'phosphor-react'
 import {Avatar} from './Avatar'
+import { useState } from 'react'
 
 export function Comment({content, onDeleteComment}){
+    const [likeCount, setLikeCount] = useState(0)
     function handleDeleteComment() {
         onDeleteComment(content)
+    }
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1)
     }
     return(
         <div className={styles.comment}>
@@ -25,9 +30,9 @@ export function Comment({content, onDeleteComment}){
                     </p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp size={24}/>
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
